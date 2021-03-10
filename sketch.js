976a8fc7aa6896e39eed3cdc5ce40fd9c1b878b9
extrodnare274 <madhav.dilip.nair@gmail.com>
2021-03-09 19:37:48 -0500

    rasengan vs chidori

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+//rasengan vs chidori
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -62,3 +62,4 @@ function draw(){
     bird.display();
     platform.display();
 }
+
